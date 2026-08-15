08e15053de1b06223c40ae20d28c90e7bed4dfaf
Shubha M R <shubha0735@gmail.com>
2026-08-15 22:43:50 +0530

    Bulk sync: 18 submissions

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.java
@@ -0,0 +1,36 @@
+
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        // Add all numbers to the set
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int longest = 0;
+
+        for (int num : set) {
+
+            // Start only if num is the beginning
+            // of a consecutive sequence
+            if (!set.contains(num - 1)) {
+
+                int current = num;
+                int length = 1;
+
+                // Find the complete sequence
+                while (set.contains(current + 1)) {
+                    current++;
+                    length++;
+                }
+
+                longest = Math.max(longest, length);
+            }
+        }
+
+        return longest;
+    }
+}
